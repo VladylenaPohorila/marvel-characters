968a244aaeab94a83c0a72b02a06dcb58944e437
Vladylena Pohorila <vladylenapohorila@Vladylenas-Air.localdomain>
2022-10-20 12:06:15 +0300

    Install md5, add key, create utilites

---
+++ app/utilits/getData.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import md5 from 'md5';
+import { BASE_URL, PUBLIC_KEY, PRIVATE_KEY } from './key-api';
+
+const generateCredentials = () => (
+    `ts=${Date.now()}&apikey=${PUBLIC_KEY}&hash=${md5(`${Date.now()}${PRIVATE_KEY}${PUBLIC_KEY}`)}`
+);
+export const BaseUrl = `${BASE_URL}characters?&${generateCredentials()}`;
+
+export const UrlCharacter = (id) => `${BASE_URL}characters/${id}?&${generateCredentials()}`;
---
+++ app/utilits/key-api.js
@@ -0,0 +1,3 @@
+export const BASE_URL = 'https://gateway.marvel.com/v1/public/';
+export const PUBLIC_KEY = '3b75b823b9992522c774e4ce04835908';
+export const PRIVATE_KEY = '39a5caff3a0a8964c125070ccdec5d172226d6d5';
